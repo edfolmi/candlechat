b2cc03eb5ce312d6463de13b3d49fb835db422bf
Edfolmi <ephraimdanielfolmi@gmail.com>
2024-01-02 20:10:50 +0100

    removed untrack files

--- chat/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .block_group_consumers import GroupBlockConsumer
-from .block_private_consumers import PrivateBlockConsumer
-
-
-websocket_urlpatterns = [
-    path('ws/block/<str:slug>/', GroupBlockConsumer.as_asgi()),
-    path('ws/privateblock/<int:userId>/', PrivateBlockConsumer.as_asgi())
-]
--- chat/tests/test_urls.py
+++
@@ -1,8 +0,0 @@
-'''
-from django.test import TestCase
-
-class TestHomeUrl(TestCase):
-    def test_home_url(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-'''
